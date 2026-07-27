a2392806a8174f2b35d5309ac70f57e6674d0669
lluzius <lluzius@psi.de>
2026-07-27 14:11:37 +0200

    updated treesitter base config

--- lua/plugins/editor/treesitter.lua
+++ lua/plugins/editor/treesitter.lua
@@ -1,15 +1,45 @@
-return { -- Highlight, edit, and navigate code
+return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'main',
   lazy = false,
   build = ':TSUpdate',
-  main = 'nvim-treesitter.config', -- Sets main module to use for opts
-  opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
-  },
+  config = function()
+    local ts = require 'nvim-treesitter'
+
+    -- 1. Define and install your preferred parsers
+    local parsers = {
+      'bash',
+      'c',
+      'diff',
+      'html',
+      'lua',
+      'luadoc',
+      'markdown',
+      'markdown_inline',
+      'query',
+      'vim',
+      'vimdoc',
+    }
+    ts.install(parsers)
+
+    -- 2. Automatically enable Treesitter features for installed parsers
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('treesitter_setup', { clear = true }),
+      callback = function(args)
+        -- Check if a treesitter parser is registered/available for this filetype
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if lang then
+          -- Enable syntax highlighting
+          vim.treesitter.start()
+
+          -- Enable folds
+          vim.wo[0][0].folexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo[0][0].foldmethod = 'expr'
+
+          -- Enable indentation
+          vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end,
+    })
+  end,
 }
