c85571311301f2051036e917147e2e74d0322bbf
lluzius <lluzius@psi.de>
2026-07-27 22:58:24 +0200

    fix autotag and update blink

--- lua/plugins/editor/blink-cmp.lua
+++ lua/plugins/editor/blink-cmp.lua
@@ -1,30 +1,12 @@
 return { -- Autocompletion
   'saghen/blink.cmp',
-  event = 'VimEnter',
-  version = '1.*',
   dependencies = {
-    -- Snippet Engine
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = (function()
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
-        return 'make install_jsregexp'
-      end)(),
-      dependencies = {
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-      opts = {},
-    },
-    'folke/lazydev.nvim',
+    'saghen/blink.lib',
+    'rafamadriz/friendly-snippets',
   },
+  build = function()
+    require('blink.cmp').build():pwait()
+  end,
   opts = {
     keymap = {
       preset = 'enter',
@@ -35,45 +17,28 @@ return { -- Autocompletion
     },
 
     completion = {
-      documentation = { auto_show = true, auto_show_delay_ms = 200 },
-    },
-
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'lazydev' },
-      providers = {
-        lsp = {
-          transform_items = function(ctx, items)
-            local filetype = vim.bo[ctx.buf].filetype
-            for _, item in ipairs(items) do
-              local label = item.label or ''
-              local detail = item.detail or ''
-              local doc = (type(item.documentation) == 'table' and item.documentation.value) or item.documentation or ''
-              local label_desc = item.labelDetails and (item.labelDetails.description or item.labelDetails.detail) or ''
-
-              -- Combine all text fields for classification
-              local context_text = detail .. ' ' .. doc .. ' ' .. label_desc
-
-              if filetype == 'java' then
-                -- Prioritize java.util.List over java.awt.List
-                if label == 'List' then
-                  if context_text:find 'java%.util' then
-                    item.score_offset = (item.score_offset or 0) + 100
-                  elseif context_text:find 'java%.awt' then
-                    item.score_offset = (item.score_offset or 0) - 100
-                  end
-                end
-              end
-            end
-            return items
-          end,
-        },
-        lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+      menu = { border = 'rounded' },
+      documentation = { auto_show = true, auto_show_delay_ms = 200, window = { border = 'rounded' } },
+      ghost_text = { enabled = true },
+      keyword = {
+        range = 'full',
       },
     },
 
-    snippets = { preset = 'luasnip' },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+        snippets = {
+          opts = {
+            friendly_snippets = true,
+          },
+        },
+      },
+    },
 
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    snippets = { preset = 'default' },
+
+    fuzzy = { implementation = 'rust' },
 
     signature = { enabled = true },
   },
