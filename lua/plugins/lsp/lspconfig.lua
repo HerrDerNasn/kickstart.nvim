b33e71bb13ff5eda8bae15e45a627c2ed3fc6889
lluzius <lluzius@psi.de>
2026-07-27 23:36:18 +0200

    removed noice in favor of snacks notifier module

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -22,6 +22,19 @@ return {
         vim.lsp.config('jdtls', {
           settings = {
             java = {
+              autobuild = { enabled = false },
+              maxConcurrentBuilds = 1,
+              completion = { maxResults = 50 },
+              referencesCodeLens = { enabled = false },
+              implementationCodeLens = { enabled = false },
+              import = {
+                gradle = {
+                  offline = { enabled = true },
+                  arguments = { '--configure-on-demand' },
+                },
+              },
+              maven = { downloadSources = false },
+              eclipse = { downloadSources = false },
               configuration = {
                 runtimes = {
                   {
--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -11,7 +11,59 @@ return {
     words = { enabled = true, debounce = 10 },
     scroll = { enabled = true },
     scope = { enabled = true },
+    notifier = {
+      enabled = true,
+      timeout = 3000,
+    },
   },
+  config = function(_, opts)
+    require('snacks').setup(opts)
+
+    -- LSP Progress Notifications
+    ---@type table<number, {token:lsp.ProgressToken, msg:string, done:boolean}[]>
+    local progress = vim.defaulttable()
+    vim.api.nvim_create_autocmd('LspProgress', {
+      ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+      callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
+        if not client or type(value) ~= 'table' then
+          return
+        end
+        local p = progress[client.id]
+
+        for i = 1, #p + 1 do
+          if i == #p + 1 or p[i].token == ev.data.params.token then
+            p[i] = {
+              token = ev.data.params.token,
+              msg = ('[%3d%%] %s%s'):format(
+                value.kind == 'end' and 100 or value.percentage or 100,
+                value.title or '',
+                value.message and (' **%s**'):format(value.message) or ''
+              ),
+              done = value.kind == 'end',
+            }
+            break
+          end
+        end
+
+        local msg = {} ---@type string[]
+        progress[client.id] = vim.tbl_filter(function(v)
+          return table.insert(msg, v.msg) or not v.done
+        end, p)
+
+        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+        vim.notify(table.concat(msg, '\n'), 'info', {
+          id = 'lsp_progress',
+          title = client.name,
+          opts = function(notif)
+            notif.icon = #progress[client.id] == 0 and ' '
+              or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+          end,
+        })
+      end,
+    })
+  end,
   keys = {
     {
       '<leader>go',
@@ -42,6 +94,27 @@ return {
       end,
       desc = 'Prev Reference',
       mode = { 'n', 't' },
+    },
+    {
+      '<leader>el',
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = 'Open event [l]og',
+    },
+    {
+      '<leader>ee',
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = 'Open [e]rror log',
+    },
+    {
+      '<leader>un',
+      function()
+        Snacks.notifier.hide()
+      end,
+      desc = 'Dismiss [N]otifications',
     },
   },
 }
